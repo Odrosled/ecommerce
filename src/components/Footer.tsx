9358d7943612cdb3ef233eecdb4c72c3f59c18d4
Lorenzo Del Sordo <lorenzodelsordo1994@gmail.com>
2023-10-17 17:19:55 +0200

    add watermark

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 const Footer = () => {
   return (
     <>
-      <div className="mx-auto py-10">
-        Copyright © 2019, All rights reserved.
+      <div className="mx-auto py-10">Copyright © 2019, All rights reserved.</div>
+      <div className="fixed bottom-2 right-2 text-xs bg-teal-500 rounded-full p-4">
+        <p className="font-semibold">Made with ❤️ by Lorenzo Del Sordo</p>
       </div>
     </>
   );
